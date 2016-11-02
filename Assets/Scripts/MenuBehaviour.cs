6394622f7b6b8963a5dafc0cc0196d0392a46bb5
Jackson Lo <7jackson.lo@gmail.com>
2016-11-02 02:17:17 -0700

    Menu Scene and NetworkManager

---
+++ Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public enum MenuButton {
+	Play,
+	Exit
+}
+
+public class MenuBehaviour : MonoBehaviour {
+
+	public MenuButton button;
+	public GameObject loadingImage;
+
+	// Use this for initialization
+	void Start () {
+		loadingImage.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		switch (button) {
+		case MenuButton.Play:
+			loadingImage.SetActive(true);
+			SceneManager.LoadScene ("Maze");
+			break;
+		case MenuButton.Exit:
+			break;
+		}
+	}
+}
